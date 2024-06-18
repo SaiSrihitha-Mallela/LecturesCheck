5a1f890d934bbc2fd57a18b17e475245f9dd98c8
Saisrihitha.M <139038259+Saisrhitha@users.noreply.github.com>
2024-06-18 15:51:22 +0530

    4th commit

--- backend/routes/AllRoutes.js
+++ backend/routes/AllRoutes.js
@@ -1,164 +1,3 @@
-// let express = require("express");
-// const { User, Pdf, Video } = require("../Models/AllSchema");
-// const fs = require("fs");
-// const path = require("path");
-// const multer = require('multer');
-
-// let allroutes = express.Router();
-// const bcrypt = require('bcrypt');
-// allroutes.use(express.json());
-// allroutes.use("/files", express.static("files"));
-// allroutes.use("/videos", express.static("videos"));
-
-
-
-
-// allroutes.get('/', (req, res) => {
-//     console.log(" reached root");
-//     res.send("wellcome to dune lms");
-// });
-
-// //login routs/////////////////////////////////////////////////////////////////////////////////////////////////
-// allroutes.post('/login', async (req, res) => {
-//     console.log("reached login");
-//     const { username, password } = req.body;
-//     console.log(username, password);
-//     try {
-//         const user = await User.findOne({ username });
-//         console.log(user);
-//         if (!user) {
-
-//             console.log("ivalid username");
-//             return res.status(400).json({ message: 'Invalid username or password' });
-//         }
-//         console.log("check password: ", password, user.password)
-//         // const isMatch = await bcrypt.compare(password, user.password);
-//         if (password !== user.password) {
-//             console.log("invalid password");
-//             return res.status(400).json({ message: 'Invalid username or password' });
-//         }
-//         console.log("login is succefull")
-//         console.log(username, password);
-//         return res.status(200).json({ message: 'Login successful' });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: 'Server error' });
-
-//     }
-// });
-
-
-
-// //pdf routes///////////////////////////////////////////////
-
-// const { default: mongoose } = require("mongoose");
-// const status = require("statuses");
-// const { all } = require("proxy-addr");
-
-
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, './files');
-//         console.log("u reached backend file");
-//     },
-//     filename: function (req, file, cb) {
-//         const uniqueSuffix = Date.now();
-//         cb(null, uniqueSuffix + file.originalname);
-//         console.log("you reacedbackend file");
-
-//     },
-// });
-
-
-// const upload = multer({ storage: storage });
-// allroutes.post("/uploadfiles", upload.single("file"), async (req, res) => {
-//     console.log(req.file);
-//     const title = req.body.title;
-//     const fileName = req.file.filename;
-//     try {
-//         await Pdf.create({
-//             title: title,
-//             pdf: fileName
-//         });
-//         res.json({ status: "ok" })
-//     } catch (error) {
-//         console.error("Error uploading file:", error);
-//         res.status(500).json({ status: "error", message: "Failed to upload file" });
-
-//     }
-
-
-// });
-
-
-// allroutes.get("/getfiles", async (req, res) => {
-//     try {
-//         Pdf.find({}).then((data) => {
-//             res.send({ status: "ok", data: data });
-
-//         });
-//     } catch (error) {
-
-//     }
-// });
-
-
-// /////////////////video///////////////////
-// // const{defalut:mongoose}=require("mongoose");
-// // const status = require("statuses");
-// // const{all} = require("proxy-addr");
-
-// const Uploadstorage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, './videos');
-//         console.log("u reached backend video file");
-//     },
-//     filename: function (req, file, cb) {
-//         const uniqueSuffix = Date.now();
-//         cb(null, uniqueSuffix + file.originalname);
-//         console.log("you reacedbackend video file");
-
-//     },
-// })
-
-// const Upload = multer({ storage: Uploadstorage });
-// allroutes.post("/Uploadvideos", Upload.single("file"), async (req, res) => {
-
-//     console.log(req.file);
-//     const title = req.body.title;
-//     const fileName = req.file.filename;
-//     try {
-//         await Video.create({
-//             title: title,
-//             video: fileName
-//         });
-//         res.json({ status: "ok" })
-//     } catch (error) {
-//         console.error("Error Uploading file:", error);
-//         res.status(500).json({ status: "error", message: "Failed to upload file" });
-//     }
-
-// });
-
-// allroutes.get("/getvideos", async (req, res) => {
-//     try {
-//         Video.find({}).then((data) => {
-//             res.send({ status: "ok", data: data });
-//         });
-
-//     } catch (error) {
-
-//     }
-// });
-
-
-
-
-
-
-// module.exports = allroutes;
-
-
 const express = require("express");
 const { User, Pdf, Video } = require("../Models/AllSchema");
 const fs = require("fs");
@@ -176,110 +15,10 @@ allroutes.use(express.json());
 allroutes.use("/files", express.static("files"));
 allroutes.use("/videos", express.static("videos"));
 
-ffmpeg.setFfmpegPath('C:/ffmpeg/ffmpeg-master-latest-win64-gpl/ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe'); // Adjust the path as needed
 
-const convertPdfToText = async (pdfPath) => {
-    try {
-        const dataBuffer = fs.readFileSync(pdfPath);
-        const pdf = await PDFParser(dataBuffer);
-        const text = pdf.text;
-        return text;
-    } catch (error) {
-        console.error("Error converting PDF to text:", error);
-        throw error;
-    }
-};
 
-const writeFile = (filePath, data) => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(filePath, data, "utf8", (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
-    });
-};
 
-const convertVideoToAudio = (inputVideo, outputAudio) => {
-    return new Promise((resolve, reject) => {
-        ffmpeg(inputVideo)
-            .noVideo()
-            .audioCodec("libmp3lame")
-            .save(outputAudio)
-            .on("end", () => {
-                console.log("Conversion complete");
-                resolve(outputAudio);
-            })
-            .on("error", (err) => {
-                console.error("Error converting video to audio:", err);
-                reject(err);
-            });
-    });
-};
-
-const uploadAudioToAssemblyAI = async (filePath) => {
-    const formData = new FormData();
-    formData.append("audio", fs.createReadStream(filePath));
-    const Keys = " 4f1b7c3dc56140459d05a750e1e2563a";
-    try {
-        const response = await axios.post("https://api.assemblyai.com/v2/upload", formData, {
-            headers: {
-                "authorization": Keys,
-                ...formData.getHeaders(),
-            },
-        });
-        return response.data.upload_url;
-    } catch (error) {
-        console.error("Error uploading audio to AssemblyAI:", error);
-        throw error;
-    }
-};
-
-const transcribeAudioToText = async (audioUrl) => {
-    const config = { audio_url: audioUrl };
-    const Keys =" 4f1b7c3dc56140459d05a750e1e2563a";
-
-    try {
-        const response = await axios.post("https://api.assemblyai.com/v2/transcript", config, {
-            headers: {
-                "authorization":Keys,
-            },
-        });
-        const transcriptId = response.data.id;
-
-        let transcript;
-        while (true) {
-            const transcriptResponse = await axios.get(`https://api.assemblyai.com/v2/transcript/${transcriptId}`, {
-                headers: {
-                "authorization": Keys,
-            },
-      });
-        transcript = transcriptResponse.data;
-
-        if (transcript.status === "completed") {
-            break;
-        } else if (transcript.status === "failed") {
-            throw new Error("Transcription failed");
-        }
-
-        await new Promise((resolve) => setTimeout(resolve, 5000));
-    }
-
-    return transcript.text;
-} catch (error) {
-    console.error("Error transcribing audio to text:", error);
-    throw error;
-}
-};
-
-allroutes.get("/", (req, res) => {
-    console.log("reached root");
-    res.send("welcome to dune lms");
-});
-
-// Login route
+///////// Login route////////////////////
 allroutes.post("/login", async (req, res) => {
     console.log("reached login");
     const { username, password } = req.body;
@@ -305,7 +44,41 @@ allroutes.post("/login", async (req, res) => {
     }
 });
 
-// PDF upload and retrieval routes
+
+
+
+//////////ffmpeg connection///////////////////////////////////////////////////////
+ffmpeg.setFfmpegPath('C:/ffmpeg/ffmpeg-master-latest-win64-gpl/ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe'); // Adjust the path as needed
+
+////////////pdf to text converion////////////////////////////////////
+const convertPdfToText = async (pdfPath) => {
+    try {
+        const dataBuffer = fs.readFileSync(pdfPath);
+        const pdf = await PDFParser(dataBuffer);
+        const text = pdf.text;
+        return text;
+    } catch (error) {
+        console.error("Error converting PDF to text:", error);
+        throw error;
+    }
+};
+
+const writeFile = (filePath, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filePath, data, "utf8", (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+};
+
+
+
+
+//////////// PDF upload and retrieval routes///////////////////////////////
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "./files");
@@ -354,7 +127,88 @@ allroutes.get("/getfiles", async (req, res) => {
     }
 });
 
-// Video upload and retrieval routes
+
+////////////video to text conversion /////////
+const convertVideoToAudio = (inputVideo, outputAudio) => {
+    return new Promise((resolve, reject) => {
+        ffmpeg(inputVideo)
+            .noVideo()
+            .audioCodec("libmp3lame")
+            .save(outputAudio)
+            .on("end", () => {
+                console.log("Conversion complete");
+                resolve(outputAudio);
+            })
+            .on("error", (err) => {
+                console.error("Error converting video to audio:", err);
+                reject(err);
+            });
+    });
+};
+
+const uploadAudioToAssemblyAI = async (filePath) => {
+    const formData = new FormData();
+    formData.append("audio", fs.createReadStream(filePath));
+    const Keys = " 4f1b7c3dc56140459d05a750e1e2563a";
+    try {
+        const response = await axios.post("https://api.assemblyai.com/v2/upload", formData, {
+            headers: {
+                "authorization": Keys,
+                ...formData.getHeaders(),
+            },
+        });
+        return response.data.upload_url;
+    } catch (error) {
+        console.error("Error uploading audio to AssemblyAI:", error);
+        throw error;
+    }
+};
+
+const transcribeAudioToText = async (audioUrl) => {
+    const config = { audio_url: audioUrl };
+    const Keys = " 4f1b7c3dc56140459d05a750e1e2563a";
+
+    try {
+        const response = await axios.post("https://api.assemblyai.com/v2/transcript", config, {
+            headers: {
+                "authorization": Keys,
+            },
+        });
+        const transcriptId = response.data.id;
+
+        let transcript;
+        while (true) {
+            const transcriptResponse = await axios.get(`https://api.assemblyai.com/v2/transcript/${transcriptId}`, {
+                headers: {
+                    "authorization": Keys,
+                },
+            });
+            transcript = transcriptResponse.data;
+
+            if (transcript.status === "completed") {
+                break;
+            } else if (transcript.status === "failed") {
+                throw new Error("Transcription failed");
+            }
+
+            await new Promise((resolve) => setTimeout(resolve, 5000));
+        }
+
+        return transcript.text;
+    } catch (error) {
+        console.error("Error transcribing audio to text:", error);
+        throw error;
+    }
+};
+
+allroutes.get("/", (req, res) => {
+    console.log("reached root");
+    res.send("welcome to dune lms");
+});
+
+
+
+// Video upload and retrieval routes///////////////////////////////////
 const uploadStorage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "./videos");
